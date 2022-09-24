d6e5df4ab09e0756aa6a7899ecaf4cad3fdc1e97
hayashi masafumi <masafumin1515@outlook.jp>
2022-09-25 00:06:39 +0900

    E_1_10_20

---
+++ template.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define all(x) (x).begin(),(x).end()
+using namespace std;
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int INF = INT_MAX / 2; const ll INFL = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+void _main() {
+  
+}
