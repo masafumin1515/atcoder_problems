7572aa8f7d1ec163e2d03969427cc44c8a3f2c40
hayashi masafumi <masafumin1515@outlook.jp>
2022-10-02 15:21:48 +0900

    Easy_70

--- easy_100/70_C_100_to_105.cpp
+++ easy_100/70_C_100_to_105.cpp
@@ -1,13 +1,24 @@
 #include<bits/stdc++.h>
 #define rep(i,a,b) for(int i=a;i<b;i++)
-#define rrep(i,a,b) for(int i=a;i>=b;i--)
-#define all(x) (x).begin(),(x).end()
 using namespace std;
 void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
-typedef long long ll; const int INF = INT_MAX / 2; const ll INFL = 1LL << 60;
-template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
-template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
 
+int X;
+int A[6] = {100,101,102,103,104,105};
+bool dp[7][100010];
+// -------------
 void _main() {
-  
+  cin >> X;
+
+  dp[0][0] = 1;
+  rep(j,1,X+1) dp[0][j] = 0;
+
+  rep(i,0,6) {
+    rep(j,0,X+1) {
+      dp[i+1][j] = dp[i][j]; // 0個選択
+      if ( j-A[i] >= 0 ) dp[i+1][j] = max(dp[i+1][j], dp[i+1][j-A[i]]); // 0個選択 or 1個以上選択
+    }
+  }
+
+  cout << dp[6][X] << endl;
 }
