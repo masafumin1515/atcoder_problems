ecaa54e58cd3cd9d52a0fc375cd5ec2c64fbe148
hayashi masafumi <masafumin1515@outlook.jp>
2022-10-06 09:11:24 +0900

    update_README

--- template.cpp
+++ template.cpp
@@ -1,7 +1,9 @@
 #include<bits/stdc++.h>
 #define rep(i,a,b) for(int i=a;i<b;i++)
 #define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a) // for(auto &x : vec)
 #define all(x) (x).begin(),(x).end()
+#define all_arr(x) begin(x), end(x)
 using namespace std;
 void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
 typedef long long ll; const int INF = INT_MAX / 2; const ll INFL = 1LL << 60;
