0e0b09838b6d1e9054359fbf2a6d51fa87d505a2
hayashi masafumi <masafumin1515@outlook.jp>
2022-10-21 23:43:11 +0900

    55_tmp

---
+++ medium_100/55.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#define rep(i,a,b) for(int i=a;i<b;i++)
+#define rrep(i,a,b) for(int i=a;i>=b;i--)
+#define fore(i,a) for(auto &i:a) // for(auto &x : vec)
+#define all(x) (x).begin(),(x).end()
+#define all_arr(x) begin(x), end(x)
+using namespace std;
+void _main(); int main() { cin.tie(0); ios::sync_with_stdio(false); _main(); }
+typedef long long ll; const int INF = INT_MAX / 2; const ll INFL = 1LL << 60;
+template<class T>bool chmax(T& a, const T& b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } return 0; }
+
+int N,M,from_1[201010]={};
+void _main() {
+  cin >> N >> M;
+  // rep(i,0,)
+}
