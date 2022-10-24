1ec74bb4cde6c9bcabf9faf8645e25bfcd0f98e4
hayashi masafumi <masafumin1515@outlook.jp>
2022-10-24 21:42:07 +0900

    setup

---
+++ Hard/template.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+#define all(x) (x).begin(),(x).end()
+#define all_arr(x) begin(x), end(x)
+using namespace std;
+typedef long long ll; const int INF = INT_MAX / 2; const ll INFL = 1LL << 60;
+
+int main() {
+  
+}
