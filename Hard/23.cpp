2959c9d6161d39b35739ae48f791a83f310b51f4
hayashi masafumi <masafumin1515@outlook.jp>
2022-11-05 12:57:18 +0900

    23

--- Hard/23.cpp
+++ Hard/23.cpp
@@ -1,16 +1,20 @@
 #include<bits/stdc++.h>
-typedef long long ll; const int INF = INT_MAX / 2; const ll INFL = 1LL << 60;
 using namespace std;
 
-int pos;
-string S,ans,strs[2]={"dream","erase"};
+int pos; // 文字列取り出し開始位置
+string S,ans;
 // ----------------------
 int main() {
   cin >> S;
+
+  pos=0;
   ans="YES";
   for (;pos<(int)S.size();) {
     string str=S.substr(pos,5);
-    if (str == strs[0] || str == strs[1]) {
+    if (str == "dream") {
+      pos += 5;
+      if ("er" == S.substr(pos,2) && "era" != S.substr(pos,3)) pos += 2;
+    } else if (str == "erase") {
       pos += 5;
       if ("r" == S.substr(pos,1)) pos += 1;
     } else {
