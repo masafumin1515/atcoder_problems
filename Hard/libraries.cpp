4f35d379dbac92ad32f1cfc6719b39f81744d7ff
hayashi masafumi <masafumin1515@outlook.jp>
2022-11-14 17:56:57 +0900

    update_libraries

--- Hard/libraries.cpp
+++ Hard/libraries.cpp
@@ -12,26 +12,26 @@ template<class T>bool chmin(T& a, const T& b) { if (b < a) { a = b; return 1; } 
 
 // 約数列挙
 vector<long long> enum_divisors(long long N) {
-  vector<long long> res;
-  for (long long i = 1; i * i <= N; ++i) {
-    if (N % i == 0) {
-      res.push_back(i);
-      // 重複しないならば i の相方である N/i も push
-      if (N/i != i) res.push_back(N/i);
+    vector<long long> res;
+    for (long long i = 1; i * i <= N; ++i) {
+        if (N % i == 0) {
+        res.push_back(i);
+        // 重複しないならば i の相方である N/i も push
+        if (N/i != i) res.push_back(N/i);
+        }
     }
-  }
-  // 小さい順に並び替える
-  sort(res.begin(), res.end());
-  return res;
+    // 小さい順に並び替える
+    sort(res.begin(), res.end());
+    return res;
 }
 
 // 素数判定
 bool is_prime(long long N) {
-  if (N == 1) return false;
-  for (long long i = 2; i * i <= N; ++i) {
-    if (N % i == 0) return false;
-  }
-  return true;
+    if (N == 1) return false;
+    for (long long i = 2; i * i <= N; ++i) {
+        if (N % i == 0) return false;
+    }
+    return true;
 }
 
 
@@ -39,24 +39,24 @@ bool is_prime(long long N) {
 // 1 以上 N 以下の整数が素数かどうかを返す
 vector<bool> Eratosthenes(int N) {
   // テーブル
-  vector<bool> isprime(N+1, true);
+    vector<bool> isprime(N+1, true);
 
-  // 1 は予めふるい落としておく
-  isprime[1] = false;
+    // 1 は予めふるい落としておく
+    isprime[1] = false;
 
-  // ふるい
-  for (int p = 2; p <= N; ++p) {
-    // すでに合成数であるものはスキップする
-    if (!isprime[p]) continue;
+    // ふるい
+    for (int p = 2; p <= N; ++p) {
+        // すでに合成数であるものはスキップする
+        if (!isprime[p]) continue;
 
-    // p 以外の p の倍数から素数ラベルを剥奪
-    for (int q = p * 2; q <= N; q += p) {
-      isprime[q] = false;
+        // p 以外の p の倍数から素数ラベルを剥奪
+        for (int q = p * 2; q <= N; q += p) {
+        isprime[q] = false;
+        }
     }
-  }
 
-  // 1 以上 N 以下の整数が素数かどうか
-  return isprime;
+    // 1 以上 N 以下の整数が素数かどうか
+    return isprime;
 }
 
 
@@ -84,4 +84,45 @@ long long COM(int n, int k){
     if (n < k) return 0;
     if (n < 0 || k < 0) return 0;
     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
+}
+
+
+// mod. m における a の逆元 a^{-1} を計算する
+// mとaは互いに素
+long long modinv(long long a, long long m) {
+    long long b = m, u = 1, v = 0;
+    while (b) {
+        long long t = a / b;
+        a -= t * b; swap(a, b);
+        u -= t * v; swap(u, v);
+    }
+    u %= m;
+    if (u < 0) u += m;
+    return u;
+}
+
+// 拡張ユークリッドの互除法から
+// 返り値: a と b の最大公約数
+// ax + by = gcd(a, b) を満たす (x, y) が格納される
+long long extGCD(long long a, long long b, long long &x, long long &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    long long d = extGCD(b, a%b, y, x);
+    y -= a/b * x;
+    return d;
+}
+
+// 二分累乗法、繰り返し累乗法
+// a^n mod を計算する
+long long modpow(long long a, long long n, long long mod) {
+    long long res = 1;
+    while (n > 0) {
+        if (n & 1) res = res * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return res;
 }
